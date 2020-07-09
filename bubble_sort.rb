98a003c46eaa02fcfaa5db247164750a27a2c070
tenlayers <itstenlayers@gmail.com>
2020-07-09 23:55:01 +0300

    Remove dumb comments

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,14 +1,5 @@
 # frozen_string_literal: true
 
-# ------------------------------------
-#
-# >  bubble_sort([4, 3, 78, 2, 0, 2])
-# => [0, 2, 2, 3, 4, 78]
-#
-# ------------------------------------
-
-# require 'byebug'
-
 array = [4, 3, 78, 2, 0, 2]
 
 def bubble_sort(array)
